f215a6ffb3775ab95752fc1a30c833914547aec9
Rafael Andrade <rafael.io@outlook.com>
2024-02-11 10:43:05 -0300

    improve structure header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,8 @@
 'use client';
-import Image from 'next/image';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
+import UseProfile from './UserProfile';
+
 import { useEffect, useState } from 'react';
 
 export default function Header() {
@@ -26,32 +29,11 @@ export default function Header() {
       className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6`}
     >
       <div className='flex items-center space-x-2 md:space-x-4'>
-        <Image
-          loader={({ src }) => `https://rb.gy/${src}`}
-          src='ulxxee'
-          alt='netflix'
-          width={120}
-          height={120}
-        />
-        <ul className='hidden md:flex md:space-x-4'>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-        </ul>
+        <Logo />
+        <NavLinks />
       </div>
 
-      <div className='flex items-center space-x-4 md:space-x-8'>
-        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-        <Image
-          loader={({ src }) => `https://rb.gy/${src}`}
-          src='g1pwyx'
-          alt=''
-          width={35}
-          height={35}
-          className='cursor-pointer rounded'
-        />
-      </div>
+      <UseProfile />
     </div>
   );
 }
