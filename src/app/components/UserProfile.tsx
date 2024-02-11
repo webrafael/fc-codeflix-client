57d17ba31ad2567426e13546b1f915e4950386b6
Rafael Andrade <rafael.io@outlook.com>
2024-02-11 10:42:40 -0300

    create component user-profile

---
+++ src/app/components/UserProfile.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+export default function UserProfile() {
+  const profileName = 'Kids';
+
+  return (
+    <div className='flex items-center space-x-4 md:space-x-8'>
+      <p className='hidden cursor-not-allowed lg:inline'>{profileName}</p>
+      <Image
+        src='/profile.png'
+        alt={`${profileName}`}
+        width={35}
+        height={35}
+        className='cursor-pointer rounded'
+      />
+    </div>
+  );
+}
