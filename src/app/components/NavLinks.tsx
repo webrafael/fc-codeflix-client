900fd0e6e066cfbe3547d2938d0debb6e76a07ba
Rafael Andrade <rafael.io@outlook.com>
2024-02-11 10:42:30 -0300

    create component nav-links

---
+++ src/app/components/NavLinks.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NavLinks() {
+  return (
+    <ul className='hidden md:flex md:space-x-4'>
+      <li>Home</li>
+      <li>TV Shows</li>
+      <li>Movies</li>
+      <li>Latest</li>
+    </ul>
+  );
+}
