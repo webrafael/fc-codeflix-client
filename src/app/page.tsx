4f76c261c12729de13297020e9e04a2502b233cb
Rafael Andrade <rafael.io@outlook.com>
2024-02-11 11:00:03 -0300

    fixes import header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
-import Header from './components/Header.1';
+import Header from './components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
