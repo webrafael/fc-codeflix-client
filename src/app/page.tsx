033bb07b71409eae875fa3109257b3ea4438ef83
Rafael Andrade <rafael.io@outlook.com>
2024-02-11 13:03:07 -0300

    improve architecture components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google';
-import Image from 'next/image';
 import Header from './components/Header';
+
+import { Banner } from './components/Banner';
+import { MoveRow } from './components/MoveRow';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,33 +11,10 @@ export default function Home() {
     <div className='relative h-full overflow-hidden bg-gradient-to-b from-transparent to-black to-[75%] lg:h-[140vh]'>
       <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
-        <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
-            <Image
-              src='/banner.png'
-              alt='MAID'
-              fill={true}
-              className='h-[65vh] object-cover object-top lg:h-[95vh]'
-            />
-          </div>
-
-          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>MAID</h1>
-
-          <p className='text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl'>
-            After fleeing an abusive relationship, a young mother finds a job
-            cleaning houses as she fights to probide for her child and build
-            them a better future.
-          </p>
-        </div>
-
-        <div className='flex space-x-3'>
-          <button className='felx cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
-            Play
-          </button>
-          <button className='felx cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-white transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
-            More Info
-          </button>
-        </div>
+        <Banner />
+        <MoveRow sectionTitle='Trending Now' />
+        <MoveRow sectionTitle='Top Rated' />
+        <MoveRow sectionTitle='Action Movies' />
       </main>
     </div>
   );
